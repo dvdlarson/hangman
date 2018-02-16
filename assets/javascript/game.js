e8c4e6645e34f0dd3fc2cc3e9530319b424f666f
unknown <webd3vdave@gmail.com>
2018-02-16 13:08:39 -0700

    game play working, mobile working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,13 @@
-var currentMaskValue;
-var targetWord;
+
+
+
+var currentMaskValue="";
+var targetWord="";
 var targetObject;
 var targetID;
 var guessCount = 0;
 var guessedLetter;
+var guessWord="";
 var wrongGuessCount=0;
 var guessRemaining=6;
 var wrongGuesses = [];
@@ -11,6 +15,10 @@ var totalGuesses = [];
 var guessedArray = [];
 var winCount = 0;
 var lossCount = 0;
+var winPic="";
+var winSound="";
+var bkgrnd="";
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 function elm(elementID) {
     return document.getElementById(elementID);
@@ -21,21 +29,42 @@ function elm(elementID) {
 
 function initializeGame() {
 
+elm("playAgainButton").setAttribute("style","display:none");
+guessRemaining=6;
 elm("guessremaining").innerHTML=guessRemaining;
 elm("subheader").innerHTML="Press any key to guess a letter.";
-
-var wordList = [word1];
+elm("sidebarpic").src="./assets/media/questionmark.png";
+guessedArray=[];
+currentMaskValue="";
+elm("lettersguessed").innerText="";
+guessCount=0;
+wrongGuessCount=0;
+var wordList = [word1,word2];
 //, {word2}, {word3}, {word4}, {word5}, {word6}, {word7}
 var rand = Math.floor(Math.random() * wordList.length);
- targetObject = wordList[rand];
+targetObject = new Object(wordList[rand]);
+ console.log("game initialized: target object:" + targetObject);
+ targetID;
  targetWord = targetObject.value;
-elm("maskvalue").innerHTML=targetObject.maskValue;
+ currentMaskValue=targetObject.maskValue;
+ winPic=targetObject.winPic;
+ bkgrnd=targetObject.bkgrnd;
+ winSound=targetObject.winSound;
+ console.log("target id: " + targetID);
+ console.log("target winpic: " + winPic);
+ console.log("target bkrd: " + bkgrnd);
+ console.log("target winsound: " + winSound);
+
+
+elm("maskvalue").innerHTML=currentMaskValue;
 console.log("Hey you unrepentant cheating bum, the answer is: "+targetWord);
-console.log("target obj: " + targetObject)
-currentMaskValue=targetObject.maskValue;
+console.log("target obj: " + targetObject);
 console.log("maskvalue: " + currentMaskValue);
+gameStart();
 
 };
+
+//gamestart by clicking letter in keyboard layout
 
 function selectLetter(letter) {
     guessedLetter=letter;
@@ -52,13 +81,11 @@ function gameStart(letter,targetObject) {
     guessedLetter=letter;
   //  guessedLetter=changeCase(letter); //convert letter to upper case
   //  console.log("converted letter: " + guessedLetter);
-    if (checkIfGuessed(guessedLetter)) {return;};//if letter has been guessed, do nothing
+    if (checkIfGuessed(guessedLetter,guessedArray)) {return;};//if letter has been guessed, do nothing
     guessedArray.push(guessedLetter);//push guessed letter into guessed letter array
     elm("lettersguessed").innerHTML=guessedArray;//replace guessed letter section with updated guessed letter array
-    guessCount++;
-    guessRemaining--;
-    elm("guessremaining").innerHTML=guessRemaining;//update guesses remaining section
-    checkAndReplace(guessedLetter);
+    guessCount++;//increment total guess counter
+    currentMaskValue=checkAndReplace(guessedLetter,targetWord,currentMaskValue);//check target word for guessed letter and update the masked value array wherever it is found.
     if (guessRemaining===0) {  //check to see if any guesses are remaining
         endGame("lose");
     };
@@ -76,15 +103,26 @@ function changeCase(element) {
     element.value=element.value.toUpperCase();
 };
 
+function setCharAt(str,index,chr) {
+    if(index > str.length-1) return str;
+    return str.substr(0,index) + chr + str.substr(index+1);
+};
 
-function checkAndReplace(guessedLetter) 
+function checkAndReplace(guessedLetter,targetWord,currentMaskValue) 
 {
-    var replaceCount=0
-    for (i=0;i<targetWord.length;i++)
-    { 
+    console.log("init C&R guessedLetter: "+guessedLetter);
+    console.log("init C&R targetWord: "+targetWord);
+    console.log("init C&R maskvalue: "+currentMaskValue);
+    
+    
+    var replaceCount=0;
+    
+    for (var i=0;i<targetWord.length;i++)
+    { console.log("char: "+targetWord.charAt(i));
         if (targetWord.charAt(i)===guessedLetter) 
-        {
-            currentMaskValue[i]=guessedLetter;
+        {   console.log("match");
+            var x = setCharAt(currentMaskValue,i,guessedLetter);
+            currentMaskValue = x;
             replaceCount++;
             elm("maskvalue").innerHTML=currentMaskValue;
         
@@ -95,11 +133,14 @@ function checkAndReplace(guessedLetter)
     if (replaceCount===0) 
         {
             wrongGuessCount++;
+            guessRemaining--;
+            elm("guessremaining").innerHTML=guessRemaining;//update guesses remaining section
         };
     console.log("new mask value after guess: " + currentMaskValue);
+    return currentMaskValue;
 };
 
-function checkIfGuessed(guessedLetter) {
+function checkIfGuessed(guessedLetter,guessedArray) {
     for (i=0; i<guessedArray.length;i++)
     {
         if (guessedArray[i]===guessedLetter) {
@@ -111,7 +152,7 @@ function checkIfGuessed(guessedLetter) {
     }   
 };
 
-function endGame(result) 
+function endGame(result,targetObject) 
 {
 
     wrongGuesses.push(wrongGuessCount);
@@ -120,35 +161,61 @@ function endGame(result)
     totalGuesses.push(guessCount);
     var avgGuess=average(totalGuesses);
     elm("avgguesses").innerHTML="Average # of guesses per win: " + avgGuess;
+    elm("playAgainButton").setAttribute("style","display:block");
 
     if (result==="win") 
         {
-            var audio = new Audio(targetObject.winSound);
-            audio.play;
-            elm("sidebarpic").src=targetObject.winPic;
-            elm("sidebarbkgrnd").src=targetObject.bkgrnd;
-            elm("subheader").innerHTML="You win! The answer was: " + targetWord;
             winCount++;
+            elm("sidebarbkgrnd").src=bkgrnd;
+            elm("sidebarpic").src = winPic;
+            elm("subheader").innerHTML="You win! The answer was: " + targetWord;
+            var sound = document.createElement("audio");
+            sound.src=winSound;
+            sound.play();
+           // alert("You Win!!");
             elm("wincount").innerHTML="Total wins: " + winCount;
+    
+          //  nextGame();
+          //  initializeGame();
+           // playAgain();
         };
 
     if (result==="lose") 
         {
-            var audio = new Audio("../media/sadtrombone.mp3");
-            audio.play;
-            elm("sidebarpic").src=targetObject.winPic;
-            elm("sidebarbkgrnd").src=targetObject.bkgrnd;           
-            elm("subheader").innerHTML="You lose! The answer was: " + targetWord;
             lossCount++;
+
+            elm("sidebarpic").src="./assets/media/youlose.png";
+
+            var audio = new Audio("./assets/media/sadtrombone.mp3");
+            audio.play();
+            
+          //  elm("sidebarbkgrnd").src=bkgrnd;  
+            alert("You Lose!")         
+            elm("subheader").innerHTML="You lose! The answer was: " + targetWord;
+            
             elm("losscount").innerHTML="Total Losses: " + lossCount;
+            nextGame();
+            initializeGame();
+           
         };
 
+        function nextGame() {
+            for (var i=3;i>0,i--;) 
+            {
+            elm("subheader").innerHTML="Next game begins in: " + i;
+            wait(1000);
+            }
+        }
+
+/* function playAgain() {
     wait(2000);
-    var playAgain=alert("Would you like to play again?");
-    if (playAgain===true) {initializeGame();}
+    var playAgain=confirm("Would you like to play again?");
+    if (playAgain===true) {
+        initializeGame();
+        }
     else {location.reload();};
 
-    
+    };   */
     
 };
 
@@ -169,5 +236,7 @@ function wait(ms){
     while(end < start + ms) {
       end = new Date().getTime();
    }
- }
+ };
+
+
 
--- assets/javascript/words.js
+++ assets/javascript/words.js
@@ -3,7 +3,16 @@ var word1 = {
     id:word1,
     value:"HORSE",
     maskValue:"_____",
-    winPic:"../media/horse.png",
-    bkgrnd:"../media/meadow.png",
-    winSound:"../media/horse.mp4",
-}
+    winPic:"./assets/media/horse.png",
+    bkgrnd:"./assets/media/meadow.png",
+    winSound:"./assets/media/horse.mp3"
+};
+var word2 = {
+    id:word2,
+    value:"ELEPHANT",
+    maskValue:"________",
+    winPic:"./assets/media/elephant.png",
+    bkgrnd:"./assets/media/jungle.png",
+    winSound:"./assets/media/elephant.mp3"
+};
+
